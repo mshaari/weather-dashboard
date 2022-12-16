70ca6f4a9504f4fb185b722839c6c2d53d99ecf7
mshaari <michael.shaari@gmail.com>
2022-12-16 13:16:12 -0500

    Somewhat functional JS and minor styling

---
+++ Assets/script.js
@@ -0,0 +1,76 @@
+var searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || []; //declare this outside of a function so that it's a universal variable
+
+var latFromCityName; //declare these two variables as universal so they can be accessed/changed throughout
+var lonFromCityName;
+
+$('#searchCityButton').on('click', function () {    
+    
+    var cityName = $('#cityName').val();
+
+    var geocodingURL = 'http://api.openweathermap.org/geo/1.0/direct?q=' + cityName + '&limit=5&appid=dcf204ce377ddb8eb2328c6723f67b46';
+    
+    $.ajax({
+    url: geocodingURL,
+    method: 'GET',
+    }).then(function (response) {
+        console.log(response);
+        latFromCityName = response.lat;
+        lonFromCityName = response.lon;
+    });
+});
+
+$('#searchLatLonButton').on('click', function () {    
+    $(".weatherSegment").empty(); //clears past weather segment
+    $("#nameOfCityEntered").empty(); //clears the title of the city from the previous search
+
+    var lat = $('#lat').val();
+    var lon = $('#lon').val();
+
+
+
+    var openWeatherURL = 'https://api.openweathermap.org/data/2.5/forecast?lat='+ lat + '&lon=' + lon + '&units=imperial&appid=dcf204ce377ddb8eb2328c6723f67b46'; //NEED TO SOMEHOW ADD LAT AND LON IN HERE
+
+
+    $.ajax({
+    url: openWeatherURL,
+    method: 'GET',
+    }).then(function (response) {
+        console.log(response);
+        var cityFromCoordinates = response.city.name;
+        $(`<h1>Weather for ${cityFromCoordinates}</h2>`).appendTo('#nameOfCityEntered'); //sometimes shows no result
+
+        searchHistory.push(cityFromCoordinates);
+        localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
+
+        for (var i=0; i<5; i++) {
+            var dayID= document.getElementById("day" + [i]);
+            var dateHeader = document.createElement("h2");
+            dateHeader.textContent = i + ' day(s) in the future';
+            dayID.appendChild(dateHeader);
+
+            //display weather icon
+            weatherIcon= response.list[i].weather[0].icon;
+            weatherIconURL='http://openweathermap.org/img/wn/' + weatherIcon + '@2x.png';
+            $(`<img src='${weatherIconURL}'>`).appendTo(dayID);
+
+            //display temp
+            temp= response.list[i].main.temp;
+            $(`<p>Temperature is ${temp} \u00B0F</p>`).appendTo(dayID);
+
+            //display wind
+            wind= response.list[i].wind.speed;
+            $(`<p>Wind is ${wind} mph</p>`).appendTo(dayID);
+
+            //display humidity
+            humidity= response.list[i].main.humidity;
+            $(`<p>Humidity is ${humidity}%</p>`).appendTo(dayID);
+        };
+    });
+});
+
+for (i = 0; i < 5; i++) { 
+    var searchHistoryElement = document.createElement("li");
+    searchHistoryElement.textContent = searchHistory[i];
+    var searchHistoryBlock = document.getElementById('searchHistory');
+    searchHistoryBlock.appendChild(searchHistoryElement);
+}
